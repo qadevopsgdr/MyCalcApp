f3563db588db80a7742dac694d2668644948fc56
root <root@ip-10-0-1-168.ap-south-1.compute.internal>
2025-07-30 06:18:31 +0000

    Calc Files - 1st version

---
+++ script.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch(e) {
+        alert("Invalid Calculation");
+    }
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+
